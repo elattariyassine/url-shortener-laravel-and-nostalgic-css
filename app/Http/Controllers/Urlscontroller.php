3cfe06c039b45df40436f76f127d012a0774bb93
elattariyassine <45572720+ELATTARIYassine@users.noreply.github.com>
2021-10-28 02:20:43 +0100

    remove comments

--- app/Http/Controllers/Urlscontroller.php
+++ app/Http/Controllers/Urlscontroller.php
@@ -12,77 +12,32 @@ class Urlscontroller extends Controller
     {
     	return view('welcome');
     }
+    
     public function store(Request $request)
     {
-
-    // TOUT CA FONCTIONNE
-	/*request('url')*/
-	/*request()->get('url')*/
-	/*$url = request('url');*/
-
-	
- 
- 	//autre methode pour la validation
- 	$this->validate($request,['url' => 'required|url']);
- 	/*$data  = ['url' => $url];
+	$this->validate($request,['url' => 'required|url']);
 	$validation = Validator::make($data,['url' => 'required|url'])->validate();*/
-	/*
-		['required'=>'Ce champ est requis','url' => 'URL invalide'])	
-	*/
-	/*we can do this in record with this condition
-	if ($validation->fails()) {
-		dd('Failed');
-	}*/
 	$record = $this->getRecordForUrl($request->url);
-
-	if (!$record) {
-		return view('result')->with('shortened',$record->shortened);
-	}
-	else
-	{
-		return view('error');
-	}
-
-
-	
-
-	
+        if (!$record) {
+            return view('result')->with('shortened',$record->shortened);
+        }
+        else
+        {
+            return view('error');
+        }
 	}
 
 
 	public function show($shortened)
     {
-    $url = Url::where('shortened',$shortened)->firstOrFail();
+        $url = Url::where('shortened',$shortened)->firstOrFail();
 
     	return redirect($url->url);
-    
 	}
 
 	private function getRecordForUrl($url)
 	{
-
-	/*a = Url::firstOrCreate*/
-	/*difference btwn them is New doesnt save it in data base u need to do this after
-	$a->save();*/
-
-	return Url::firstOrCreate(['url' => $url],['shortened'=>Url::get_unique_url_short_url()]);
-							 //The first arrow mean
-							 //condition if u found
-							 //this url return him
-							 //if not create him and add the 2nd argument to the table
-
-	/*$record = Url::where('url',$url)->first();
-
-		if ($record) 
-		{
-		return $record;
-		}
-
-	return Url::create([
-			'url' 		=> $url,
-			'shortened' => Url::get_unique_url_short_url()
-		]);*/
-	
+        return Url::firstOrCreate(['url' => $url],['shortened'=>Url::get_unique_url_short_url()]);
 	}
 
 }
